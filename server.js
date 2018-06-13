c0fa62a553d4177f92e1d5425accf09bf74b1927
Ian Hill <kamarja@gmail.com>
2018-06-13 14:39:54 -0400

    project sync

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log('listening on 3000');
+});
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+*/
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  console.log(__dirname)
+})
